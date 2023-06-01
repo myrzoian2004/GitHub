f73b00eb7487246a7b2d11bfccdee017f63fac96
Myrzoian <borismerzaan@gmail.com>
2023-06-01 13:54:00 +0300

    createproject

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n1 = 0;
+            int n2 = 1;
+            int n3;
+            Console.WriteLine(n1 + "\n" + n2);
+            for (int i = 0; i < 38; i++)
+            {
+                n3 = n1 + n2;
+                n1 = n2;
+                n2 = n3;
+                Console.WriteLine(n3);
+            }
+            Console.ReadLine();
+        }
+    }
+}
